1d8d77c476074cc99148c79d43f1c44a67d57c43
Diego Fernández Criado <dfc25@icloud.com>
2024-02-15 09:23:28 +0100

    ejemplo 13 Select

---
+++ ejemplos/select/ejemplo13.sql
@@ -0,0 +1,42 @@
+/*
+ * Supongamos ahora que queremos obtener los nombres de los empleados que están alojados
+ * en ‘MULLERS’ o ‘PAPA KING’. Realizaremos esta consulta mediante la técnica de
+ * subconsultas utilizando los operadores anteriores. La consulta puede escribirse de la
+ * siguiente manera:
+*/
+
+SELECT
+    NOMBRE
+FROM
+    EMPLEADO
+WHERE
+    ALOJAMIENTO= ANY(
+        SELECT
+            ALOJAMIENTO_ID
+        FROM
+            ALOJAMIENTO
+        WHERE
+            ALOJAMIENTO IN( 'MULLERS', 'PAPA KING' )
+    );
+
+-- CON JOIN
+
+SELECT
+    NOMBRE
+FROM
+    EMPLEADO
+    JOIN ALOJAMIENTO
+    ON EMPLEADO.ALOJAMIENTO=ALOJAMIENTO_ID
+WHERE
+    ALOJAMIENTO.ALOJAMIENTO IN ('MULLERS', 'PAPA KING');
+
+-- CONSULTA NORMAL
+
+SELECT
+    NOMBRE
+FROM
+    EMPLEADO,
+    ALOJAMIENTO
+WHERE
+    EMPLEADO.ALOJAMIENTO=ALOJAMIENTO_ID
+    AND ALOJAMIENTO.ALOJAMIENTO IN ('MULLERS', 'PAPA KING');
--- temp.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO COMPANIA VALUES (2,'Manolito');
