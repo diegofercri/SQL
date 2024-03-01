c819f7ec20bef92fd343e9200465e36ca1f9615b
Diego Fernández Criado <dfc25@icloud.com>
2024-03-01 09:55:23 +0100

    Examen 01 MAR Ejercicios 4 y 5

--- examen/ejercicio4.sql
+++ examen/ejercicio4.sql
@@ -1,5 +1,16 @@
 /*
-    Muestra el nombre del oficio de todos los oficios (sin repetir) y cuantos empleados lo ejercen.
+    Muestra el nombre de todos los oficios (sin repetir) y cuantos empleados lo ejercen.
     En oficios sin empleados mostrar 0.
 */
 
+SELECT
+    O.OFICIO,
+    COUNT(E.NUMEMP) AS "CANTIDAD DE EMPLEADOS"
+FROM
+    OFICIO O
+    LEFT JOIN OFICIOEMPLEADO OE
+    ON O.NUMOFICIO = OE.OFICIO
+    LEFT JOIN EMPLEADO E
+    ON OE.EMPLEADO = E.NUMEMP
+GROUP BY
+    O.OFICIO;
---
+++ examen/ejercicio5.sql
@@ -0,0 +1,19 @@
+/*
+    Mostrar el arbol resultante de la consulta jerarquica sobre trabajador con raiz 'KING',
+    no incluir trabajadores/jefes cuyo nombre contenga 'n' o 'm' en su tercer caracter.
+*/
+
+-- NO ESTA TERMINADO
+
+SELECT
+    LPAD(' ', 2*(LEVEL-1))
+    ||NOMBRE AS TRABAJADOR,
+    COD_EMP,
+    COD_JEFE
+FROM
+    TRABAJADOR START WITH NOMBRE='KING'
+CONNECT BY
+    PRIOR COD_EMP=COD_JEFE
+-- ESTA PARTE NO FUNCIONA
+WHERE
+    INSTR(NOMBRE, 'N', 3, 1)<>3;
